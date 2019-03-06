7a864c33358834d5cc508b34b7cd1a43863686bd
Cheekyfunkymonkey <shigechu@gmail.com>
2019-03-06 14:08:39 +1300

    do exercise

--- main.js
+++ main.js
@@ -14,7 +14,24 @@
  *         - username : 返信ツイートしたユーザー名(文字列。ユーザー名は自由につけて良い)
  *         - text : ツイート内容(文字列。ツイート内容は自由に作って良い。)
  */
-
+const tweets = [
+  {
+    username: "太郎",
+    text: "こんにちは",
+    replies: [
+      { username: "花子", text: "いい天気だね" },
+      { username: "一郎", text: "今日もよろしく" }
+    ]
+  },
+  {
+    username: "二郎",
+    text: "明日は雨らしいよ",
+    replies: [
+      { username: "みー", text: "にゃ！？" },
+      { username: "太郎", text: "傘もってかないとね" }
+    ]
+  }
+];
 
 /**
  * 課題2: 課題1で作ったtweetsの内容を全て表示する
@@ -24,3 +41,14 @@
  *     - replies
  *       - repliesの内容もforEachを使って全て表示する
  */
+tweets.forEach((tweet, index) => {
+  console.log("-----------ツイート" + index + "-----------");
+  console.log("ユーザー名：　", tweet.username);
+  console.log("ツイート：　", tweet.text);
+
+  tweet.replies.forEach((reply, index) => {
+    console.log("-----------返信" + index + "-----------");
+    console.log("ユーザー名：　", reply.username);
+    console.log("ツイート：　", reply.text);
+  });
+});
